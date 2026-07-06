775e092b122faceb775ad4d5332f17ea85d3af08
apex001x <e.e.edu3355@gmail.com>
2026-07-07 00:33:33 +0900

    chore: update CORS origins for production frontend

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://todo-list-iohl.onrender.com"
+    ],
     allow_methods=["*"],
     allow_headers=["*"],
 )
